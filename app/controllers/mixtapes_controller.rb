8d8df61793967644406ebf0562e6e46b1e2949c8
phaltoe <reverse9@live.com>
2016-07-25 13:20:17 -0300

    added strong params

--- app/controllers/mixtapes_controller.rb
+++ app/controllers/mixtapes_controller.rb
@@ -3,4 +3,21 @@ class MixtapesController < ApplicationController
   def new
     @mixtape = Mixtape.new
   end
+
+  def create
+    @mixtape = Mixtape.new(mixtape_params)
+
+    if @mixtape.valid?
+      @mixtape.save
+      raise "OwoW Yeah!"
+    else
+      raise "BoooOh!"
+    end
+  end
+
+  private
+
+  def mixtape_params
+    params.require(:mixtape).permit(:name)
+  end
 end
--- app/models/mixtape.rb
+++ app/models/mixtape.rb
@@ -1,3 +1,5 @@
 class Mixtape < ActiveRecord::Base
   has_many :songs
+
+  validates :name, presence: true
 end
